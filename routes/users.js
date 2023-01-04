250a4e43d943ce25eb19364a2fafb425d1f03434
OpenTech-Consult <opentech.consult@gmail.com>
2023-01-04 10:40:44 +0000

    Build Rest API with Node Express.
    created the get by id route, delete by id route and update by id route.

--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,7 @@ import { v4 as uuidv4 } from 'uuid'
 
 const router = express.Router()
 
-const users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 25
-    },
-    {
-        firstName: "Jane",
-        lastName: "Doe",
-        age: 22
-    },
-]
+let users = []
 
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
@@ -27,6 +16,31 @@ router.post('/', (req, res) => {
     const user = req.body
     users.push({ ...user, id: uuidv4() })
     res.send(`User with the name ${user.firstName} added to the database`)
+})
+
+router.get('/:id', (req, res) => {
+    console.log(req.params)
+    const { id } = req.params
+    const foundUser = users.find((user) => user.id === id)
+    res.send(foundUser)
+})
+
+router.delete('/:id', (req, res) => {
+    console.log(req.params)
+    const { id } = req.params
+    users = users.filter((user) => user.id !== id)
+    res.send(`User with the id ${id} delete from the database.`)
+})
+
+router.patch('/:id', (req, res) => {
+    console.log(req.params)
+    const { id } = req.params
+    const { firstName, lastName, age } = req.body
+    const user = users.find((user) => user.id === id)
+    if (firstName) user.firstName = firstName
+    if (lastName) user.lastName = lastName
+    if (age) user.age = age
+    res.send(`User with the id ${id} has been updated`)
 })
 
 
